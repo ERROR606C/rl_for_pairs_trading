7a1dd3fe8a99376ac5cb16379d7378c868db0abd
tudor <pagu.tudor@gmail.com>
2026-05-16 18:21:30 +0200

    basic env

---
+++ project/data_loader.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+
+
+class PairData:
+    def __init__(self, path: str):
+        df = pd.read_csv(path, index_col=0)
+        self.labels = df.columns.tolist()
+        self.timestamps = df.index.to_numpy(dtype=np.int64)
+        self.s = df.iloc[:, :2].to_numpy(dtype=np.float64).T  # shape (2, N)
+
+    def __len__(self):
+        return len(self.s[0])
+
+
+if __name__ == "__main__":
+    import sys
+    import matplotlib.pyplot as plt
+
+    if len(sys.argv) != 2:
+        print(f"Usage: python {sys.argv[0]} <path/to/pair.csv>")
+        sys.exit(1)
+
+    data = PairData(sys.argv[1])
+
+    fig, ax = plt.subplots(figsize=(12, 4))
+    ax.plot(data.s[0], label=data.labels[0])
+    ax.plot(data.s[1], label=data.labels[1])
+    ax.set_xlabel("Time step")
+    ax.set_ylabel("Close price")
+    ax.legend()
+    plt.tight_layout()
+    plt.show()
---
+++ project/env.py
@@ -0,0 +1,91 @@
+import gymnasium as gym
+from gymnasium import spaces
+import numpy as np
+from data_loader import PairData
+from enum import Enum
+
+class Actions(Enum):
+    Maintain = 0
+    Swap = 1
+    
+class Market(gym.Env):
+    def __init__ (self, pair_data: PairData, past_window=60, initial_capital = 10000):
+        self._initial_capital = initial_capital
+        self._capital = initial_capital
+        self._position = np.array([0,0]) #  actual current position in the real stocks
+        
+        self._pair_data = pair_data
+        self._past_window = past_window # how many ticks in the past we look to compute spread
+        self._in_position = 0 # 0 if currently outside the position, 1 if currently in the position
+        
+        self.observation_space = spaces.Dict(
+            {
+            "spread" : spaces.Box(low=-np.inf, high=np.inf, shape=(1,), dtype=np.float32)    ,
+            "position" : spaces.Discrete(2)  # 0 = flat, 1 = in position
+        })
+        
+        self.action_space = spaces.Discrete(2) # 0 -> Maintain ; 1 -> Swap your position (either enter or exit)
+
+        self._t = past_window # t is an index into the prices array. We start at window since we can't compute beta before that
+    
+    def _get_obs(self):
+        return {
+            "spread": self._get_spread(),
+            "position": self._in_position
+        }
+        
+    def _get_spread(self):
+        # Run an OLS on the past "_past_window" ticks to compute a cointegration beta between the two stocks
+        s0 = self._pair_data.s[0][self._t - self._past_window:self._t]
+        s1 = self._pair_data.s[1][self._t - self._past_window:self._t]
+        
+        beta = np.polyfit(s1, s0, 1)[0]
+        
+        spread = self._pair_data.s[0][self._t] - beta * self._pair_data.s[1][self._t]
+        
+        return np.array([spread], dtype=np.float32)
+    
+    def reset(self, seed=None):
+        super().reset(seed=seed)
+        self._t = self._past_window
+        self._in_position = 0
+        self._position = np.zeros(2)
+        self._capital = self._initial_capital
+        
+        return self._get_obs(), {}
+    
+    def _get_pnl(self):
+        # returns current portfolio value
+        return self._capital + np.dot(self._position, np.array([self._pair_data.s[0][self._t], self._pair_data.s[1][self._t]]))
+        
+    def step(self, action):
+        old_portfolio_value = self._get_pnl()
+        
+        if action == Actions.Swap.value:
+            if self._pair_data.s[0][self._t] > self._pair_data.s[1][self._t]:
+                cheap_i = 1
+                expensive_i = 0
+            else:
+                cheap_i = 0
+                expensive_i = 1
+                
+            # dollar neutral approach -> Sepnd half of our money on the short leg, half on the long leg
+            if self._in_position == 0:  # entering position
+                half = self._capital / 2
+                self._position[cheap_i] = half / self._pair_data.s[cheap_i][self._t]
+                self._position[expensive_i] = -(half / self._pair_data.s[expensive_i][self._t])
+                self._capital = 0.0
+            else:  # exiting position
+                self._capital = self._get_pnl()
+                self._position = np.zeros(2)
+                
+            self._in_position = 1 - self._in_position
+
+        self._t += 1
+        
+        terminated = self._t >= len(self._pair_data) - 1
+        new_portfolio_value = self._get_pnl()
+        reward = new_portfolio_value - old_portfolio_value
+    
+    
+        return self._get_obs(), reward, terminated, False, {}
